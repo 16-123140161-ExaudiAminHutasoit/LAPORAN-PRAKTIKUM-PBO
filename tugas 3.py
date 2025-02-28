757cd76ea6556a6dd9edc872c2616c69501565fc
16-123140161-ExaudiAminHutasoit <exaudi.123140161@student.itera.ac.id>
2025-03-01 00:36:58 +0700

    Add files via upload

---
+++ tugas 3.py
@@ -0,0 +1,9 @@
+file = open("me.txt","w")
+
+file.write("nama saya exaudi.\n")
+file.write("saya ingin bergerak dari kemalasan.\n")
+file.write("yang menyiksa.\n")
+
+file.close()
+
+print(f"file me.txt berhasil di buat")
